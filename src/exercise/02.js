8f93d5e6f34159914f0b7d6f74e10c0caa0d6474
Chris Anderson <chris.anderson@zywave.com>
2021-11-02 12:47:22 -0400

    description: useEffect - for ex 2 -

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,19 +1,19 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import * as React from "react";
 
-function Greeting({initialName = ''}) {
+function Greeting({initialName = ""}) {
   // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  initialName = window.localStorage.getItem("name") || initialName;
+  const [name, setName] = React.useState(initialName);
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem("name", name);
+  });
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
   return (
     <div>
@@ -21,13 +21,13 @@ function Greeting({initialName = ''}) {
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting />;
 }
 
-export default App
+export default App;
